574e184ceea3afb03a47b1f22fd701f8bfd0d606
Peter Benjamin <peter_benjamin@intuit.com>
2016-10-02 23:57:59 -0700

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+const petermbenjamin = {
+  contact: {
+    codewars: "https://www.codewars.com/users/pmbenjamin",
+    devpost: "http://devpost.com/pmbenjamin",
+    email: "petermbenjamin@gmail.com",
+    github: "https://github.com/pmbenjamin",
+    keybase: "https://keybase.io/pbenj",
+    linkedIn: "https://linkedin.com/in/pmbenjamin",
+    medium: "https://medium.com/@pmbenjamin",
+    npm: "https://www.npmjs.com/~pmbenjamin",
+    quora: "https://www.quora.com/profile/Peter-Benjamin-2",
+    reddit: "https://www.reddit.com/user/Pmbenjamin/",
+    twitter: "https://twitter.com/pmbenjamin",
+  },
+  name: {
+    first: "Peter",
+    last: "Benjamin",
+    middle: "M",
+  },
+}
+
+export default petermbenjamin
