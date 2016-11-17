fe100324ac6ffc6c15c91add2466a2f96eac184e
Peter Benjamin <peter_benjamin@intuit.com>
2016-11-16 20:33:56 -0800

    update cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,33 +1,33 @@
 #!/usr/bin/env node
 
-const commander = require('commander')
-const chalk = require('chalk')
-const packageJson = require('../package.json')
-const Table = require('cli-table')
-const petermbenjamin = require('../dist/index')
+const commander = require('commander');
+const chalk = require('chalk');
+const packageJson = require('../package.json');
+const info = require('../index.js');
+const Table = require('cli-table');
 
 commander.version(packageJson.version)
-  .usage('[command]')
+  .usage('[command]');
 
 commander.command('fullname').alias('f')
   .description('Print Full Name')
   .action(() => {
-    console.log(chalk.bold.underline(`${petermbenjamin.name.first} ${petermbenjamin.name.middle} ${petermbenjamin.name.last}`), '|', petermbenjamin.name.title)
-  })
+    console.log(chalk.bold.underline(`${info.name.first} ${info.name.middle} ${info.name.last}`), '|', info.name.title);
+  });
 
 commander.command('contact').alias('c')
   .description('Print Contact Info')
   .action(() => {
     const table = new Table({
       head: ['Site', 'Contact'],
-      colWidths: [10, 50]
-    })
-    for (const key in petermbenjamin.contact) {
-      table.push([key, petermbenjamin.contact[key]])
-    }
-    console.log(table.toString())
-  })
+      colWidths: [10, 50],
+    });
+    Object.keys(info.contact).forEach((key) => {
+      table.push([key, info.contact[key]]);
+    });
+    console.log(table.toString());
+  });
 
-commander.parse(process.argv)
+commander.parse(process.argv);
 
-if (!process.argv.slice(2).length) commander.outputHelp()
+if (!process.argv.slice(2).length) commander.outputHelp();
