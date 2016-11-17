e97a84f74dac6c826e64351fa559d8a1c85724a9
Peter Benjamin <peter_benjamin@intuit.com>
2016-11-16 20:34:20 -0800

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
+};
